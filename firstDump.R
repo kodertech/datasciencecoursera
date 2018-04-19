c6602ea42fafbaa9b926bade93205e2975a25b09
Kartik Sharma <kartiksharma1994@gmail.com>
2018-04-19 11:42:22 +0530

    my first Dput-Dget and Dump-Source use

---
+++ firstDump.R
@@ -0,0 +1,6 @@
+x <-
+"foo"
+y <-
+structure(list(a = 1:4, b = structure(1:4, .Label = c("a", "b", 
+"c", "d"), class = "factor")), .Names = c("a", "b"), row.names = c("r1", 
+"r2", "r3", "r4"), class = "data.frame")
